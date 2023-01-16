f37e6c98a3a0f59b3b5444f3c30daccc25bbb168
Douglas Fukuhara <douglas.fukuhara@mogo.ca>
2023-01-16 12:27:54 -0800

    Adjusting StatusBar color based on the current fragment

--- gamefeature/src/main/java/br/com/fdtechcorp/android/guessgotnames/lib/gamefeature/mainmenu/ui/MainMenuFragment.kt
+++ gamefeature/src/main/java/br/com/fdtechcorp/android/guessgotnames/lib/gamefeature/mainmenu/ui/MainMenuFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.fdtechcorp.android.guessgotnames.lib.gamefeature.R
@@ -13,6 +14,8 @@ import br.com.fdtechcorp.android.guessgotnames.lib.gamefeature.game.business.mod
 class MainMenuFragment : Fragment(R.layout.main_menu_fragment) {
     private var _binding: MainMenuFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private var previousStatusBarColor: Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,12 +29,14 @@ class MainMenuFragment : Fragment(R.layout.main_menu_fragment) {
     override fun onDestroyView() {
         super.onDestroyView()
 
+        restoreStatusBarColor()
         _binding = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        customizeStatusBarColor()
         setupGameModesButtonListeners()
     }
 
@@ -47,5 +52,18 @@ class MainMenuFragment : Fragment(R.layout.main_menu_fragment) {
     private fun navigateTo(gameMode: GameMode) {
         val action = MainMenuFragmentDirections.actionMainMenuFragmentToGuessNameGameFragment(gameMode)
         findNavController().navigate(action)
+    }
+
+    private fun customizeStatusBarColor() {
+        context?.let {
+            previousStatusBarColor = activity?.window?.statusBarColor
+            activity?.window?.statusBarColor = ContextCompat.getColor(it, br.com.fdtechcorp.android.guessgotnames.lib.designsystem.R.color.got_statusbar_main_menu_color)
+        }
+    }
+
+    private fun restoreStatusBarColor() {
+        previousStatusBarColor?.let { previousColor ->
+            activity?.window?.statusBarColor = previousColor
+        }
     }
 }
