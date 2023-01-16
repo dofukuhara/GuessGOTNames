8a2a311d9299bca80fd7e5002e3d416ec39e92ab
Douglas Fukuhara <douglas.fukuhara@mogo.ca>
2023-01-15 19:46:49 -0800

    Initial implementation of Game feature and DesignSystem modules

---
+++ gamefeature/src/main/java/br/com/fdtechcorp/android/guessgotnames/lib/gamefeature/mainmenu/ui/MainMenuFragment.kt
@@ -0,0 +1,7 @@
+package br.com.fdtechcorp.android.guessgotnames.lib.gamefeature.mainmenu.ui
+
+import androidx.fragment.app.Fragment
+import br.com.fdtechcorp.android.guessgotnames.lib.gamefeature.R
+
+class MainMenuFragment : Fragment(R.layout.main_menu_fragment) {
+}
---
+++ gamefeature/src/main/java/br/com/fdtechcorp/android/guessgotnames/lib/gamefeature/module/GameFeatureModule.kt
@@ -0,0 +1,7 @@
+package br.com.fdtechcorp.android.guessgotnames.lib.gamefeature.module
+
+import org.koin.dsl.module
+
+val gameFeatureModule = module {
+
+}
